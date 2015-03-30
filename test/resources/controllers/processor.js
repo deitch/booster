04e46e9aebb2218bfd5e87b84858994514a28850
Avi Deitcher <avi@deitcher.net>
2015-03-30 09:58:00 +0300

    Fix status code passing to post-processors

--- test/resources/controllers/processor.js
+++ test/resources/controllers/processor.js
@@ -2,28 +2,28 @@
 
 module.exports = {
 	post: {
-		all: function (req,res,next,status,body) {
+		all: function (req,res,next,body) {
 			var pr = res.get("processor"), d = pr ? [pr].concat("all") : "all", p = req.query.ptype;
 			if (p === "both" || p === "all") {
 				res.set("processor",typeof(d) === "string" ? d : JSON.stringify(d));
 			}
 			next();
 		},
-		index: function (req,res,next,status,body) {
+		index: function (req,res,next,body) {
 			var pr = res.get("processor"), d = pr ? [pr].concat("index") : "index", p = req.query.ptype;
 			if (p === "index") {
 				res.set("processor",typeof(d) === "string" ? d : JSON.stringify(d));
 			}
 			next();
 		},
-		create: function (req,res,next,status,body) {
+		create: function (req,res,next,body) {
 			var pr = res.get("processor"), d = pr ? [pr].concat("create") : "create", p = req.query.ptype;
 			if (p === "both" || p === "create") {
 				res.set("processor",typeof(d) === "string" ? d : JSON.stringify(d));
 			}
 			next();
 		},
-		update: function (req,res,next,status,body) {
+		update: function (req,res,next,body) {
 			res.status(400).send("changed");
 		}
 	}
